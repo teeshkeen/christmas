7ec5845b05e97982df6ec30cadf8fcb962461d6b
teeshkeen <lxtishkin@gmail.com>
2025-01-10 13:40:06 +0500

    added dark-theme and toggle. +swiperjs

---
+++ src/main.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const themeToggleButton = document.querySelector('.header__theme-toggle');
+  const themeIcon = themeToggleButton.querySelector('img');
+  const body = document.body;
+
+  // Проверяем, есть ли сохраненная тема в localStorage
+  const currentTheme = localStorage.getItem('theme') || 'light';
+  if (currentTheme === 'dark') {
+      body.classList.add('dark-theme');
+  }
+
+  if (body.classList.contains('dark-theme')) {
+    themeIcon.src = './src/assets/icons/sun.svg';
+  } else {
+    themeIcon.src = './src/assets/icons/moon.svg';
+  }
+
+  themeToggleButton.addEventListener('click', () => {
+      body.classList.toggle('dark-theme');
+
+      if (body.classList.contains('dark-theme')) {
+        themeIcon.src = './src/assets/icons/sun.svg';
+      } else {
+        themeIcon.src = './src/assets/icons/moon.svg';
+      }
+
+      if (body.classList.contains('dark-theme')) {
+          localStorage.setItem('theme', 'dark');
+      } else {
+          localStorage.setItem('theme', 'light');
+      }
+  });
+});
